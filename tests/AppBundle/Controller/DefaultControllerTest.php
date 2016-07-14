173b53173044453294c2e9c5c799ed5183ddf48d
Michael Lotkowski <michaell@theodo.co.uk>
2016-07-14 11:05:36 +0100

    added a failing test

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -15,4 +15,15 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
+
+    public function testHelloWorld()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/hello');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Hello World', $crawler->filter('#container h1')->text());
+
+    }
 }
